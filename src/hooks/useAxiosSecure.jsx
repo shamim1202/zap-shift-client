563e4c7d9ea3656efe0d648005784abd5ef116e1
shamim1202 <mehedi.farzu@gmail.com>
2025-12-02 00:34:29 +0600

    implement jwt token user verify server and client side

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,47 @@
 import axios from "axios";
-
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Interceptor Request...........................
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      // console.log("Request sent:", config);
+      return config;
+    });
+
+    // Interceptor Response............................
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+        const statusCode = error.status;
+        if (statusCode == 401 || statusCode == 403) {
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, logOut]);
+
   return axiosSecure;
 };
 
